e3cfd266de16ec4649377dd12ccde977e0a32aba
Roman Kavun <karomigo@gmail.com>
2020-03-25 12:52:56 +0200

    Fixed storybook

--- vue-infinity-scroll/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import {addDecorator} from '@storybook/vue';
-
-console.log('-------------------------------------------------------------------')
-
-import vuetify from '../src/plugins/vuetify';
-addDecorator(() => ({
-  vuetify,
-  template: '<v-app><story/></v-app>'
-}))
--- vue-infinity-scroll/stories/InfinityTest.stories.js
+++ vue-infinity-scroll/stories/InfinityTest.stories.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
+import vuetify from 'vuetify';
+
 import InfinityTest from '../src/components/InfinityTest.vue';
+
+Vue.use(vuetify);
 
 export default {
   title: 'InfinityTest',
